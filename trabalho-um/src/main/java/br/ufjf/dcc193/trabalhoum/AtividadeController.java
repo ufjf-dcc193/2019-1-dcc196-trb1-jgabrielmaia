1be87ff8ae831ef9757ebfb8f83da1427fbb9f84
J. Gabriel Maia <jonagabriel@gmail.com>
2019-04-21 19:00:01 -0300

    Adicionando controllers e routing de atividade

---
+++ trabalho-um/src/main/java/br/ufjf/dcc193/trabalhoum/AtividadeController.java
@@ -0,0 +1,27 @@
+package br.ufjf.dcc193.trabalhoum;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AtividadeController {
+    @RequestMapping("/atividade/criar")
+    public String criar(){
+        return "atividade/criar";
+    }
+
+    @RequestMapping("/atividade/editar")
+    public String editar(){
+        return "atividade/editar";
+    }
+
+    @RequestMapping("/atividade/excluir")
+    public String excluir(){
+        return "atividade/excluir";
+    }
+
+    @RequestMapping("/atividade/listar")
+    public String listar(){
+        return "atividade/listar";
+    }
+}
