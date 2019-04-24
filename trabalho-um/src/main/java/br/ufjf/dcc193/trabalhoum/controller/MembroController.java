8b88b98b67cd1d41e8d00ed5cf023a13037ded8d
J. Gabriel Maia <jonagabriel@gmail.com>
2019-04-24 01:29:47 -0300

    MVC ready: listar membros

--- trabalho-um/src/main/java/br/ufjf/dcc193/trabalhoum/controller/MembroController.java
+++ trabalho-um/src/main/java/br/ufjf/dcc193/trabalhoum/controller/MembroController.java
@@ -1,10 +1,21 @@
 package br.ufjf.dcc193.trabalhoum.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.ufjf.dcc193.trabalhoum.data.MembroRepository;
+import br.ufjf.dcc193.trabalhoum.model.Membro;
 
 @Controller
 public class MembroController {
+    
+    @Autowired
+    MembroRepository repo;
+
     @RequestMapping("/membro/criar")
     public String criar(){
         return "membro/criar";
@@ -21,7 +32,12 @@ public class MembroController {
     }
 
     @RequestMapping("/membro/listar")
-    public String listar(){
-        return "membro/listar";
+    public ModelAndView listar(){
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("/membro/listar");
+
+        List<Membro> membros = repo.findAll();
+        mv.addObject("membros", membros);
+        return mv;
     }
 }
--- trabalho-um/src/main/webapp/WEB-INF/views/membro/listar.jsp
+++ trabalho-um/src/main/webapp/WEB-INF/views/membro/listar.jsp
@@ -1,4 +1,5 @@
 <%@page pageEncoding="utf-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <!DOCTYPE html>
 <html>
     <head>
@@ -6,6 +7,16 @@
         <title>ONG</title>
     </head>
     <body>
-        <h1>Listar Membro</h1>
+        <h1>Listar Membros</h1>
+            <c:forEach items="${membros}" var="membro">
+                <h2>${membro.getNome()}</h2>    
+                <ul>
+                    <li><strong>Sede:</strong> ${membro.getSede().getNome()}</li>
+                    <li><strong>Descrição:</strong> ${membro.getFuncao()}</li>
+                    <li><strong>Início da atividade:</strong> ${membro.getEmail()}</li>
+                    <li><strong>Fim da atividade:</strong> ${membro.getDataEntrada()}</li>
+                    <li><strong>Horas Assistenciais:</strong> ${membro.getDataSaida()}</li>
+                </ul> 
+            </c:forEach>        	   
     </body>
 </html>
